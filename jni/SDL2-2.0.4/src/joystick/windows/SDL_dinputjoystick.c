cdfefa2ab6c7e98b18091067d4563508c8eef5ee
William Hartman <william.a.hartman@gmail.com>
2016-08-27 21:44:34 -0400

    SDL Updates

--- jni/SDL2-2.0.4/src/joystick/windows/SDL_dinputjoystick.c
+++ jni/SDL2-2.0.4/src/joystick/windows/SDL_dinputjoystick.c
@@ -37,8 +37,8 @@
 #define AXIS_MAX    32767   /* maximum value for axis coordinate */
 #define JOY_AXIS_THRESHOLD  (((AXIS_MAX)-(AXIS_MIN))/100)   /* 1% motion */
 
-///* external variables referenced. */
-//extern HWND SDL_HelperWindow;
+/* external variables referenced. */
+// extern HWND SDL_HelperWindow;
 
 /* local variables */
 static SDL_bool coinitialized = SDL_FALSE;
@@ -240,11 +240,23 @@ SDL_IsXInputDevice(const GUID* pGuidProductFromDirectInput)
     static GUID IID_ValveStreamingGamepad = { MAKELONG(0x28DE, 0x11FF), 0x0000, 0x0000, { 0x00, 0x00, 0x50, 0x49, 0x44, 0x56, 0x49, 0x44 } };
     static GUID IID_X360WiredGamepad = { MAKELONG(0x045E, 0x02A1), 0x0000, 0x0000, { 0x00, 0x00, 0x50, 0x49, 0x44, 0x56, 0x49, 0x44 } };
     static GUID IID_X360WirelessGamepad = { MAKELONG(0x045E, 0x028E), 0x0000, 0x0000, { 0x00, 0x00, 0x50, 0x49, 0x44, 0x56, 0x49, 0x44 } };
+    static GUID IID_XOneWiredGamepad = { MAKELONG(0x045E, 0x02FF), 0x0000, 0x0000, { 0x00, 0x00, 0x50, 0x49, 0x44, 0x56, 0x49, 0x44 } };
+    static GUID IID_XOneWirelessGamepad = { MAKELONG(0x045E, 0x02DD), 0x0000, 0x0000, { 0x00, 0x00, 0x50, 0x49, 0x44, 0x56, 0x49, 0x44 } };
+    static GUID IID_XOneNewWirelessGamepad = { MAKELONG(0x045E, 0x02D1), 0x0000, 0x0000, { 0x00, 0x00, 0x50, 0x49, 0x44, 0x56, 0x49, 0x44 } };
+    static GUID IID_XOneSWirelessGamepad = { MAKELONG(0x045E, 0x02EA), 0x0000, 0x0000, { 0x00, 0x00, 0x50, 0x49, 0x44, 0x56, 0x49, 0x44 } };
+    static GUID IID_XOneSBluetoothGamepad = { MAKELONG(0x045E, 0x02E0), 0x0000, 0x0000, { 0x00, 0x00, 0x50, 0x49, 0x44, 0x56, 0x49, 0x44 } };
+    static GUID IID_XOneEliteWirelessGamepad = { MAKELONG(0x045E, 0x02E3), 0x0000, 0x0000, { 0x00, 0x00, 0x50, 0x49, 0x44, 0x56, 0x49, 0x44 } };
 
     static const GUID *s_XInputProductGUID[] = {
         &IID_ValveStreamingGamepad,
-        &IID_X360WiredGamepad,   /* Microsoft's wired X360 controller for Windows. */
-        &IID_X360WirelessGamepad /* Microsoft's wireless X360 controller for Windows. */
+        &IID_X360WiredGamepad,         /* Microsoft's wired X360 controller for Windows. */
+        &IID_X360WirelessGamepad,      /* Microsoft's wireless X360 controller for Windows. */
+        &IID_XOneWiredGamepad,         /* Microsoft's wired Xbox One controller for Windows. */
+        &IID_XOneWirelessGamepad,      /* Microsoft's wireless Xbox One controller for Windows. */
+        &IID_XOneNewWirelessGamepad,   /* Microsoft's updated wireless Xbox One controller (w/ 3.5 mm jack) for Windows. */
+        &IID_XOneSWirelessGamepad,     /* Microsoft's wireless Xbox One S controller for Windows. */
+        &IID_XOneSBluetoothGamepad,    /* Microsoft's Bluetooth Xbox One S controller for Windows. */
+        &IID_XOneEliteWirelessGamepad  /* Microsoft's wireless Xbox One Elite controller for Windows. */
     };
 
     size_t iDevice;
@@ -568,16 +580,16 @@ SDL_DINPUT_JoystickOpen(SDL_Joystick * joystick, JoyStick_DeviceData *joystickde
         return SetDIerror("IDirectInputDevice8::QueryInterface", result);
     }
 
-//    /* Acquire shared access. Exclusive access is required for forces,
-//    * though. */
-//    result =
-//        IDirectInputDevice8_SetCooperativeLevel(joystick->hwdata->
-//        InputDevice, SDL_HelperWindow,
-//        DISCL_EXCLUSIVE |
-//        DISCL_BACKGROUND);
-//    if (FAILED(result)) {
-//        return SetDIerror("IDirectInputDevice8::SetCooperativeLevel", result);
-//    }
+    /* Acquire shared access. Exclusive access is required for forces,
+    * though. */
+    // result =
+    //     IDirectInputDevice8_SetCooperativeLevel(joystick->hwdata->
+    //     InputDevice, SDL_HelperWindow,
+    //     DISCL_EXCLUSIVE |
+    //     DISCL_BACKGROUND);
+    // if (FAILED(result)) {
+    //     return SetDIerror("IDirectInputDevice8::SetCooperativeLevel", result);
+    // }
 
     /* Use the extended data structure: DIJOYSTATE2. */
     result =
