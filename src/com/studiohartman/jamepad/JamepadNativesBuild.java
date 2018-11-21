ed677b7b44a34759fea9367d54b27cc7d28213d3
Richard Smith <mercurial@fantastic.me.uk>
2018-11-21 19:42:55 +0000

    update mac build

--- src/com/studiohartman/jamepad/JamepadNativesBuild.java
+++ src/com/studiohartman/jamepad/JamepadNativesBuild.java
@@ -3,20 +3,14 @@ package com.studiohartman.jamepad;
 import com.badlogic.gdx.jnigen.*;
 import com.badlogic.gdx.jnigen.BuildTarget.TargetOs;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 
 class JamepadNativesBuild {
 
     static final String win32crossCompilePath="/usr/local/cross-tools/i686-w64-mingw32/bin/";
     static final String win64crossCompilePath="/usr/local/cross-tools/x86_64-w64-mingw32/bin/";
-
-    private static String[] merge(String[] a, String ... b) {
-        String[] n = new String[a.length + b.length];
-        System.arraycopy(a, 0, n, 0, a.length);
-        System.arraycopy(b, 0, n, a.length, b.length);
-        return n;
-    }
+    static final String minSDLversion="2.0.9";
+    static final String macLibPath ="/usr/local/lib/libSDL2.a";
 
 	public static void main(String[] args) throws Exception {
         //Deal with arguments
@@ -48,53 +42,52 @@ class JamepadNativesBuild {
         System.out.println("Building for OSX     (arg: build-OSX)     " + (buildOSX ? "ON" : "OFF"));
         System.out.println();
 
-        String sdl = "0";
-        try {
-            sdl = execCmd("sdl2-config --version").trim();
-        } catch (Exception e){
-            System.out.println("SDL must be installed and sdl2-config command must be on path.");
-            e.printStackTrace();
-        }
-        System.out.println("SDL version found: "+sdl);
-        if(compareVersions(sdl, "2.0.9")<0){
-            throw new FileNotFoundException("\n!!! SDL version must be >= 2.0.9");
-        }
-
-        //Windows build configs
+        BuildTarget lin64 = BuildTarget.newDefaultTarget(TargetOs.Linux, true);
         BuildTarget win32 = BuildTarget.newDefaultTarget(TargetOs.Windows, false);
         BuildTarget win64 = BuildTarget.newDefaultTarget(TargetOs.Windows, true);
-
-        win32.cFlags = win32.cFlags + " " +execCmd(win32crossCompilePath+"sdl2-config --cflags");
-        win32.cppFlags = win32.cFlags;
-        win32.libraries = execCmd(win32crossCompilePath+"sdl2-config --static-libs");
-
-        win64.cFlags = win64.cFlags + " " +execCmd(win64crossCompilePath+"sdl2-config --cflags");
-        win64.cppFlags = win64.cFlags;
-        win64.libraries = execCmd(win64crossCompilePath+"sdl2-config --static-libs");
-
-        //Linux build configs
-        BuildTarget lin64 = BuildTarget.newDefaultTarget(TargetOs.Linux, true);
-
-        lin64.cIncludes = new String[] {};
-        String cflags = execCmd("sdl2-config --cflags"); // "-I/usr/local/include/SDL2"
-        lin64.cFlags = lin64.cFlags + " "  + cflags;
-        lin64.cppFlags = lin64.cFlags;
-        lin64.linkerFlags = "-shared -m64";
-        String libraries = execCmd("sdl2-config --static-libs").replace("-lSDL2","-l:libSDL2.a" );
-        //"-L/usr/local/lib -Wl,-rpath,/usr/local/lib -Wl,--enable-new-dtags -l:libSDL2.a -Wl,--no-undefined -lm -ldl -lsndio -lpthread -lrt";
-        lin64.libraries = libraries;
-
-
-        //OSX build configs
         BuildTarget mac64 = BuildTarget.newDefaultTarget(TargetOs.MacOsX, true);
 
-        mac64.cIncludes = new String[] {};
-        mac64.headerDirs = new String[] {"/usr/local/include/SDL2"};
-        mac64.cFlags = "-c -Wall -O2 -arch x86_64 -DFIXED_POINT -fmessage-length=0 -fPIC -mmacosx-version-min=10.6";
-        mac64.cppFlags = mac64.cFlags;
-        mac64.linkerFlags = "-shared -arch x86_64 -mmacosx-version-min=10.6";
-        mac64.libraries = "/usr/local/lib/libSDL2.a -lm -liconv -Wl,-framework,CoreAudio -Wl,-framework,AudioToolbox -Wl,-framework,ForceFeedback -lobjc -Wl,-framework,CoreVideo -Wl,-framework,Cocoa -Wl,-framework,Carbon -Wl,-framework,IOKit -Wl,-weak_framework,QuartzCore -Wl,-weak_framework,Metal";
+        if(buildLinux) {
+            checkSDLVersion("sdl2-config", minSDLversion);
 
+            lin64.cIncludes = new String[] {};
+            String cflags = execCmd("sdl2-config --cflags"); // "-I/usr/local/include/SDL2"
+            lin64.cFlags = lin64.cFlags + " "  + cflags;
+            lin64.cppFlags = lin64.cFlags;
+            lin64.linkerFlags = "-shared -m64";
+            String libraries = execCmd("sdl2-config --static-libs").replace("-lSDL2","-l:libSDL2.a" );
+            //"-L/usr/local/lib -Wl,-rpath,/usr/local/lib -Wl,--enable-new-dtags -l:libSDL2.a -Wl,--no-undefined -lm -ldl -lsndio -lpthread -lrt";
+            lin64.libraries = libraries;
+        }
+
+        if(buildOSX){
+            checkSDLVersion("sdl2-config", minSDLversion);
+
+            mac64.cIncludes = new String[] {};
+            //mac64.headerDirs = new String[] {"/usr/local/include/SDL2"};
+            String cflags = execCmd("sdl2-config --cflags");
+            mac64.cFlags = cflags + " -c -Wall -O2 -arch x86_64 -DFIXED_POINT -fmessage-length=0 -fPIC -mmacosx-version-min=10.6";
+            mac64.cppFlags = mac64.cFlags;
+            mac64.linkerFlags = "-shared -arch x86_64 -mmacosx-version-min=10.6";
+            mac64.libraries = macLibPath +" -lm -liconv -Wl,-framework,CoreAudio -Wl,-framework,AudioToolbox -Wl,-framework,ForceFeedback -lobjc -Wl,-framework,CoreVideo -Wl,-framework,Cocoa -Wl,-framework,Carbon -Wl,-framework,IOKit -Wl,-weak_framework,QuartzCore -Wl,-weak_framework,Metal";
+                    // we cant use:
+                    //   execCmd("sdl2-config --static-libs").replace("-lSDL2","-l:libSDL2.a" )
+                    // because OSX has Clang, not GCC.  See https://jonwillia.ms/2018/02/02/static-linking for the problem
+
+        }
+
+        if(buildWindows){
+            checkSDLVersion(win32crossCompilePath+"sdl2-config", minSDLversion);
+            checkSDLVersion(win64crossCompilePath+"sdl2-config", minSDLversion);
+
+            win32.cFlags = win32.cFlags + " " +execCmd(win32crossCompilePath+"sdl2-config --cflags");
+            win32.cppFlags = win32.cFlags;
+            win32.libraries = execCmd(win32crossCompilePath+"sdl2-config --static-libs");
+
+            win64.cFlags = win64.cFlags + " " +execCmd(win64crossCompilePath+"sdl2-config --cflags");
+            win64.cppFlags = win64.cFlags;
+            win64.libraries = execCmd(win64crossCompilePath+"sdl2-config --static-libs");
+        }
 
         //Generate native code, build scripts
         System.out.println("##### GENERATING NATIVE CODE AND BUILD SCRIPTS #####");
@@ -129,6 +122,20 @@ class JamepadNativesBuild {
         System.out.println("##### PACKING NATIVES INTO .JAR #####");
         BuildExecutorFixed.executeAnt("jni/build.xml", "pack-natives");
 	}
+
+    private static void checkSDLVersion(String command, String version) throws FileNotFoundException{
+        String sdl = "0";
+        try {
+            sdl = execCmd(command+" --version").trim();
+        } catch (Exception e){
+            System.out.println("SDL must be installed and "+command+" command must be on path.");
+            e.printStackTrace();
+        }
+        System.out.println("SDL version found: "+sdl);
+        if(compareVersions(sdl, version)<0){
+            throw new FileNotFoundException("\n!!! SDL version must be >= "+version);
+        }
+    }
 
     public static String execCmd(String cmd) throws java.io.IOException {
         java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(cmd).getInputStream()).useDelimiter("\\A");
